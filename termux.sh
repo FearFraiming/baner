2129f4efd068e21ac8f057ac89edd913b61accd5
CIA <39837371+FearFraiming@users.noreply.github.com>
2019-03-31 09:51:31 -0600

    Add files via upload

---
+++ termux.sh
@@ -0,0 +1,29 @@
+apt update&&pkg upgrede&&pkg install figlet -y
+
+figlet " CIA "
+
+cd 
+
+rm -rf termux-ohmyzsh
+
+
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+
+
+cd termux-ohmyzsh
+
+
+chmod +x *
+
+bash install.sh
+
+echo -e "#########################"
+echo -e "# Porfavor resetea la app #"
+echo -e "##########################"
+
+figlet "By: CIA "
+echo -e "sh install.sh"
+
+sleep 0.3
+
+
